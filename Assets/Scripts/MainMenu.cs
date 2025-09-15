567b0caffba195f3486953fdb19dd327359a2bdc
AvocadoDisaster <and80800@gmail.com>
2025-09-14 21:39:07 -0400

    lastish

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,14 +1,17 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class MainMenu : onload
 {
     public void GoToScene(string sceneName) {
-        SceneManager.LoadScene(sceneName);
+        SceneManager.LoadScene("Level");
+        onload.Instantiate(this);
     }
 
     public void QuitApp() {
         Application.Quit();
         Debug.Log("Application has quit");
     }
+    
 }
---
+++ Assets/scripts/onload.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class onload : MonoBehaviour
+{
+    public GameObject player;
+        public GameObject  telekenisis;
+    public GameObject moveblock;
+        public GameObject map;
+    public GameObject eventing;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        onload.Instantiate(player);
+        onload.Instantiate(telekenisis);
+        onload.Instantiate(map);
+        onload.Instantiate(moveblock);
+        onload.Instantiate(eventing);
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
