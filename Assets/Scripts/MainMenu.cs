24522d5b2d39316c508acf6f4d0c8b6c2ba3c6a8
Helen Ratiani <hratiani1@student.gsu.edu>
2025-09-15 11:22:35 -0400

    uhh

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,12 +3,21 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioSource introSong;
+
     public void GoToScene(string sceneName) {
         SceneManager.LoadScene(sceneName);
+        StopMusic();
     }
 
     public void QuitApp() {
+        StopMusic();
         Application.Quit();
         Debug.Log("Application has quit");
+
+    }
+    void StopMusic()
+    {
+        introSong.Stop();
     }
 }
--- Assets/scripts/Blockmove.cs
+++ Assets/scripts/Blockmove.cs
@@ -37,11 +37,6 @@ public class Blockmove : MonoBehaviour
 
     }
     
-    
-
-    
-
-    
 
     private bool hitwall()
     {
