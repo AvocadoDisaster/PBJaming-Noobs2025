79fba52d16a80de962b09ba1fb405b5c2aff6c90
AvocadoDisaster <and80800@gmail.com>
2025-09-17 16:24:55 -0400

    Revert "yes"
    This reverts commit be838b38bd92065ae9803bd74c5a1a1073c359c9, reversing
    changes made to 33dab26d11a0fd61806e20657766b08aca7c6ea4.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,31 +1,14 @@
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : onload
+public class MainMenu : MonoBehaviour
 {
-<<<<<<< HEAD
     public void GoToScene(string sceneName) {
         SceneManager.LoadScene("Level");
-=======
-    public AudioSource introSong;
-    public void GoToScene(string sceneName)
-    {
-        SceneManager.LoadScene("Level");
-        onload.Instantiate(this);
-        StopMusic();
->>>>>>> 5a45df54f012873960deadba2bb72ceb19677a92
     }
 
-    public void QuitApp()
-    {
-        StopMusic();
+    public void QuitApp() {
         Application.Quit();
         Debug.Log("Application has quit");
     }
-    void StopMusic()
-    {
-        introSong.Stop();
-    }
-    
 }
--- Assets/scripts/Timer.cs
+++ Assets/scripts/Timer.cs
@@ -1,26 +1,19 @@
 using UnityEngine;
 using TMPro;
-using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
-    [SerializeField] string loseScene; 
     float elapsedTime = 60f;
 
     // Update is called once per frame
     void Update()
     {
-        int intElapsedTime = Mathf.Max(0, (int)elapsedTime);
+        int intElapsedTime = (int)elapsedTime;
         timerText.text = intElapsedTime.ToString();
         if (elapsedTime > 0f)
         {
             elapsedTime -= Time.deltaTime;
-        }
-        else
-        {
-            //change to lose scene
-            SceneManager.LoadScene(loseScene);
         }
     }
 
